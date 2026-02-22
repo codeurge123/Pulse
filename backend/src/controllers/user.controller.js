95b946e27e0dcfce32bcbfcd81b8074975685bd4
codeurge123 <bansalyash316@gmail.com>
2026-02-22 05:38:17 +0530

    Update user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,36 +2,73 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 
 import { User } from "../models/user.model";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const RegisterUser = asyncHandler( async (req,res) => {
+const RegisterUser = asyncHandler(async (req, res) => {
     // get user details from the frontend
-    // validate is the information given
+    // validate the information given to
     // check for user already exist or not
     // check for image is there or not 
     // if there are image then store them on cloudinary
-    // storing user info in db 
+    // create the user and storing user info in db 
     // return the response
 
 
-    const {name, email,username,password } = req.body;
+    const { name, email, username, password } = req.body;
 
-    if(
-        [name,email,username,password].some(field => field?.trim() === "") 
+    if (
+        [name, email, username, password].some(field => field?.trim() === "")
     ) {
-        throw new ApiError(401,"All field are required");
+        throw new ApiError(401, "All field are required");
     }
 
-    const user = await User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ email }, { username }]
     })
 
-    if(user) {
-        throw new ApiError(400,"User Already exist !!")
+    if (existedUser) {
+        throw new ApiError(400, "User Already exist !!")
     }
-    
-    
+
+    const avatarLocalPath = req.file?.avatar[0]?.path;
+
+    const coverImageLocalPath = req.file?.coverImage[0]?.path || "";
+
+    if (!avatarLocalPath) {
+        throw new ApiError(401, "Avatar is required");
+    }
 
 
-    
+    const avatar = uploadOnCloudinary(avatarLocalPath);
+
+    const coverImage = uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(402, "Error while uploading on cloudinary !!");
+    }
+
+
+    const user = await User.create({
+        name,
+        email,
+        username: username.toLowerCase(),
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    })
+
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Somehting went wrong while registering the user")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,createdUser,"User Registered Successfully")
+    )
 
 })
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
-const userSchema =  new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -46,9 +48,63 @@ const userSchema =  new mongoose.Schema({
         type: String,
     }
 },
-{
-    timestamps : true,
+    {
+        timestamps: true,
+    })
+
+// pre is a type of middleware
+// middleware hai to next pass karna he karna padhaa gaa
+userSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+    next();
+
 })
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+
+    return await bcrypt.compare(password, this.password)
+
+}
+
+
+// now generating token : 
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        // 1. payload
+        // 2. access token secert 
+        // 3. expiry of access token
+
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            name: this.name
+        },
+        process.env.ACCESS_TOKEN_SECRET
+        ,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+// refreshtoken mein kaam info hoti hai : b/c ye sirf login check kara ta hai
+userSchema.methods.generateRefreshToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: REFRESH_TOKEN_EXPIRY,
+        }
+    )
+}
 
 
 export const User = mongoose.model("User", userSchema);
